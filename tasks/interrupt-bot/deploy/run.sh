84faa02d28085d9c6e8a2dfb02b2a789e793bff5
Ryan Moran <rmoran@pivotal.io>
2020-03-03 15:19:33 -0500

    Adds deployment for interrupt-bot
    [#171574362]

--- tasks/interrupt-bot/deploy/run.sh
+++ tasks/interrupt-bot/deploy/run.sh
@@ -1,10 +1,25 @@
 #!/bin/bash
 
+set -x
 set -eu
 set -o pipefail
 
 function main() {
-  gcloud info
+  local project
+  project="$(echo "${SERVICE_ACCOUNT_KEY}" | jq -r .project_id)"
+
+  gcloud auth activate-service-account \
+    --key-file <(echo "${SERVICE_ACCOUNT_KEY}")
+
+  gcloud run deploy interrupt-bot \
+    --image gcr.io/cf-buildpacks/slack-delegate-bot:latest \
+    --max-instances 1 \
+    --memory "128Mi" \
+    --platform managed \
+    --set-env-vars "SLACK_TOKEN=${SLACK_TOKEN},PAIRIST_PASSWORD=${PAIRIST_PASSWORD}" \
+    --allow-unauthenticated \
+    --project "${project}" \
+    --region us-central1
 }
 
 main "${@}"
