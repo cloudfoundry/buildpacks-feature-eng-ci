a0e8cae442fa9b412b29cae16316513564470013
Ryan Moran <rmoran@pivotal.io>
2020-03-04 10:10:51 -0500

    Removes subdomain from deployment script
    [#171574238]
    
    Co-authored-by: Forest Eckhardt <feckhardt@pivotal.io>

--- tasks/slack-invitations/deploy/run.sh
+++ tasks/slack-invitations/deploy/run.sh
@@ -9,7 +9,8 @@ function main() {
   cf api api.run.pivotal.io
   cf auth "${CLIENT_ID}" "${CLIENT_SECRET}" --client-credentials
   cf target -o "${ORG}" -s "${SPACE}"
-  cf push "${APP}" -p "${APP_PATH}" --no-start -d "${DOMAIN}" --hostname "${SUBDOMAIN}"
+
+  cf push "${APP}" -p "${APP_PATH}" --no-start
   cf set-env "${APP}" INVITE_URL "${INVITE_URL}"
   cf start "${APP}"
 }
