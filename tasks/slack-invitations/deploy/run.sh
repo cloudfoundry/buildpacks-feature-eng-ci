608e3b43ce02199195c59b9ce4f3d5719923496a
Ryan Moran <rmoran@pivotal.io>
2020-03-04 09:48:30 -0500

    Adds slack.paketo.io subdomain when pushing
    [#171574238]

--- tasks/slack-invitations/deploy/run.sh
+++ tasks/slack-invitations/deploy/run.sh
@@ -9,7 +9,7 @@ function main() {
   cf api api.run.pivotal.io
   cf auth "${CLIENT_ID}" "${CLIENT_SECRET}" --client-credentials
   cf target -o "${ORG}" -s "${SPACE}"
-  cf push "${APP}" -p "${APP_PATH}" --no-start
+  cf push "${APP}" -p "${APP_PATH}" --no-start -d "${DOMAIN}" --hostname "${SUBDOMAIN}"
   cf set-env "${APP}" INVITE_URL "${INVITE_URL}"
   cf start "${APP}"
 }
