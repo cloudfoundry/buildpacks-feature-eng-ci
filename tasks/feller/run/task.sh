eb700f1e3b7f2695f13ec969abf6fce8cb5e6eff
Josh Zarrabi <jzarrabi@pivotal.io>
2020-03-12 10:36:00 -0400

    Adds mark-stale subcommand in feller pipeline
    [#171040965]
    
    Co-authored-by: Ryan Moran <rmoran@pivotal.io>

--- tasks/feller/run/task.sh
+++ tasks/feller/run/task.sh
@@ -4,7 +4,7 @@ readonly FELLER_DIR="${PWD}/feller"
 
 function main() {
   pushd "${FELLER_DIR}" > /dev/null || return
-    go run main.go \
+    go run main.go mark-stale \
       --tracker-project "${TRACKER_PROJECT}" \
       --tracker-token "${TRACKER_TOKEN}" \
       --github-token "${GITHUB_TOKEN}"
