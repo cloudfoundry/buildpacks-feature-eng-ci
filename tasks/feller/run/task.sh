afab1647d49451f9436925f3302bce01fe7bc922
Ryan Moran <rmoran@pivotal.io>
2020-03-20 12:14:20 -0400

    Refactors feller run task

--- tasks/feller/run/task.sh
+++ tasks/feller/run/task.sh
@@ -1,8 +1,22 @@
-#!/bin/bash -eu
+#!/bin/bash
+
+set -eu
+set -o pipefail
 
 readonly FELLER_DIR="${PWD}/feller"
 
+#shellcheck source=../../../util/print.sh
+source "${PWD}/ci/util/print.sh"
+
 function main() {
+  util::print::title "[task] executing"
+
+  feller::mark::stale
+}
+
+function feller::mark::stale() {
+  util::print::title "[task] * running feller mark-stale"
+
   pushd "${FELLER_DIR}" > /dev/null || return
     go run main.go mark-stale \
       --tracker-project "${TRACKER_PROJECT}" \
