17d99a63450e1fac8d38cc77b02d2425e4c33122
Ryan Moran <rmoran@pivotal.io>
2020-03-20 11:55:08 -0400

    Refactors slack-invitations pipeline/tasks

--- tasks/slack-invitations/deploy/task.sh
+++ tasks/slack-invitations/deploy/task.sh
@@ -4,15 +4,32 @@ set -eu
 set -o pipefail
 
 function main() {
-  local project service
-  project="$(echo "${SERVICE_ACCOUNT_KEY}" | jq -r .project_id)"
-  service="slack-invitations"
+  util::print::title "[task] executing"
+
+  gcloud::authenticate
+  gcloud::project::configure
+  gcloud::run::deploy
+}
+
+function gcloud::authenticate() {
+  util::print::info "[task] * authenticating with gcp"
 
   gcloud auth activate-service-account \
     --key-file <(echo "${SERVICE_ACCOUNT_KEY}")
-  gcloud config set project "${project}"
+}
 
-  gcloud run deploy "${service}" \
+function gcloud::project::configure() {
+  util::print::info "[task] * configuring project"
+
+  local project
+  project="$(echo "${SERVICE_ACCOUNT_KEY}" | jq -r .project_id)"
+  gcloud config set project "${project}"
+}
+
+function gcloud::run::deploy() {
+  util::print::info "[task] * deploying slack-invitations"
+
+  gcloud run deploy "slack-invitations" \
     --image gcr.io/cf-buildpacks/slack-invitations:latest \
     --max-instances 1 \
     --memory "128Mi" \
