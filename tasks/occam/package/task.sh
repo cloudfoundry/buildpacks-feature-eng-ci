affc3778b0896e97742dd0a7eaff412bc92dede4
Ryan Moran <rmoran@pivotal.io>
2020-04-08 16:51:46 -0400

    Adds pipeline for occam

---
+++ tasks/occam/package/task.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+
+set -eu
+set -o pipefail
+
+readonly OCCAM_DIR="${PWD}/occam"
+readonly VERSION_DIR="${PWD}/version"
+readonly ARTIFACTS_DIR="${PWD}/artifacts"
+
+#shellcheck source=../../../util/print.sh
+source "${PWD}/ci/util/print.sh"
+
+function main() {
+  util::print::title "[task] executing"
+
+  release::prepare
+}
+
+function release::prepare() {
+  util::print::info "[task] * preparing release"
+
+  local version
+  version="$(cat "${VERSION_DIR}/version")"
+
+  printf "v%s" "${version}" > "${ARTIFACTS_DIR}/name"
+  printf "v%s" "${version}" > "${ARTIFACTS_DIR}/tag"
+
+  pushd "${PACKIT_DIR}" > /dev/null || return
+    git rev-parse HEAD > "${ARTIFACTS_DIR}/commitish"
+  popd > /dev/null || return
+}
+
+main "${@}"
