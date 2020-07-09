2c23e7649d386a45a0e12213f3d0dbca8f7d7b16
Josh Zarrabi <jzarrabi@vmware.com>
2020-07-09 22:05:46 +0000

    add output to cnb/dispatch task

--- tasks/cnb/dispatch/task.sh
+++ tasks/cnb/dispatch/task.sh
@@ -3,11 +3,18 @@
 set -eu
 set -o pipefail
 
+#shellcheck source=../../../util/print.sh
+source "${PWD}/ci/util/print.sh"
+
 function main() {
+  util::print::title "[task] executing"
+
   dispatch::send
 }
 
 function dispatch::send() {
+  util::print::info "[task] * sending dispatch to tanzu-${BUILDPACK}"
+
   local version
   version=$(cat release/version)
 
