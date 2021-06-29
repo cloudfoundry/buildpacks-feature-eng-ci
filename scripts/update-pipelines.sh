1e6b2731266639bca25b2e89d43d24ba7eab4408
Ryan Moran <rmoran@pivotal.io>
2021-06-29 11:52:29 -0700

    Removes unused pipelines and tasks

--- scripts/update-pipelines.sh
+++ scripts/update-pipelines.sh
@@ -4,7 +4,8 @@ set -e
 set -u
 set -o pipefail
 
-readonly ROOT_DIR="$(cd "$(dirname "$0")" && cd .. && pwd)"
+ROOT_DIR="$(cd "$(dirname "$0")" && cd .. && pwd)"
+readonly ROOT_DIR
 
 function main() {
   local include
@@ -48,7 +49,7 @@ USAGE
 }
 
 function team::check() {
-  if ! string::contains "$(yq r ~/.flyrc targets.buildpacks.team)" "feature-eng" ; then
+  if ! string::contains "$(yq eval .targets.buildpacks.team ~/.flyrc )" "feature-eng" ; then
     echo "Refusing to update pipelines. Please log in as the 'feature-eng' team using:"
     echo -e "\n  fly -t buildpacks login -n feature-eng\n"
     exit 1
@@ -60,37 +61,16 @@ function pipelines::update() {
   local include
   include="${1}"
 
-  local basic_pipelines cloudfoundry_cnb_pipelines shim_pipelines
+  local basic_pipelines
   basic_pipelines=(
-    builder-images
     ci-images
-    cnb2cf
     feller
     interrupt-bot
     slack-invitations
   )
-  cloudfoundry_cnb_pipelines=(
-    nodejs-compat-cnb
-    php-compat-cnb
-    python-compat-cnb
-  )
-  shim_pipelines=(
-    go-shim
-    nodejs-shim
-    php-shim
-    python-shim
-  )
 
   for name in "${basic_pipelines[@]}"; do
     pipeline::update::basic "${name}" "${include}"
-  done
-
-  for name in "${cloudfoundry_cnb_pipelines[@]}"; do
-    pipeline::update::cnb::legacy "${name}" "${include}"
-  done
-
-  for name in "${shim_pipelines[@]}"; do
-    pipeline::update::shim "${name}" "${include}"
   done
 }
 
@@ -113,69 +93,6 @@ function pipeline::update::basic() {
       set-pipeline \
         --pipeline "${name}" \
         --config "${ROOT_DIR}/pipelines/${name}.yml"
-    echo
-  fi
-}
-
-function pipeline::update::cnb() {
-  local name include
-  name="${1}"
-  include="${2}"
-
-  if string::contains "${name}" "${include}"; then
-    echo "=== UPDATING ${name} ==="
-    fly --target buildpacks \
-      set-pipeline \
-        --pipeline "${name}" \
-        --config <(
-          ytt \
-            --file "${ROOT_DIR}/pipelines/cnb/template.yml" \
-            --file "${ROOT_DIR}/pipelines/cnb/config.yml" \
-            --data-value buildpack="${name}" \
-            --data-value github_token_name="paketo-buildpacks-github-token" \
-            --data-value github_org="paketo-buildpacks"
-        )
-    echo
-  fi
-}
-
-function pipeline::update::cnb::legacy() {
-  local name include
-  name="${1}"
-  include="${2}"
-
-  if string::contains "${name}" "${include}"; then
-    echo "=== UPDATING ${name} ==="
-    fly --target buildpacks \
-      set-pipeline \
-        --pipeline "${name}" \
-        --config <(
-          ytt \
-            --file "${ROOT_DIR}/pipelines/cnb/template.yml" \
-            --file "${ROOT_DIR}/pipelines/cnb/config.yml" \
-            --data-value buildpack="${name%-cnb}" \
-            --data-value suffix="-cnb"
-        )
-    echo
-  fi
-}
-
-function pipeline::update::shim() {
-  local name include
-  name="${1}"
-  include="${2}"
-
-  if string::contains "${name}" "${include}"; then
-    echo "=== UPDATING ${name} ==="
-    fly --target buildpacks \
-      set-pipeline \
-        --pipeline "${name}" \
-        --config <(
-          ytt \
-            --file "${ROOT_DIR}/pipelines/shim/template.yml" \
-            --file "${ROOT_DIR}/pipelines/shim/config.yml" \
-            --data-value buildpack="${name%-shim}"
-        )
     echo
   fi
 }
